9234b73d6b5d526bf207098095613822d9f3963b
Elias Bandoh <ebandoh27@gmail.com>
2025-11-10 12:04:08 +0000

    Sorting the dates into ascending order

--- week_8/price_sort.py
+++ week_8/price_sort.py
@@ -5,16 +5,20 @@ import matplotlib.pyplot as plt
 # Load the Palantir price data from a CSV file
 palantir_price_data_df = pd.read_csv('HistoricalData_1762772785970.csv')
 
-#show all rows
-pd.set_option('display.max_rows', None)
 
-print(palantir_price_data_df)
+# sorting the data by date in ascending order
+palantir_price_data_df['Date'] = pd.to_datetime(palantir_price_data_df['Date'], format='%m/%d/%Y')
+palantir_price_data_df = palantir_price_data_df.sort_values(by='Date', ascending=True)
 
 # calculate daily price change
 
 palantir_price_data_df['Close/Last'] = palantir_price_data_df['Close/Last'].replace('[\$,]', '', regex=True).astype(float) # Remove dollar signs and convert to float
 
 palantir_price_data_df['Daily Price Change'] = palantir_price_data_df['Close/Last'].diff()
+
+
+#show all rows
+pd.set_option('display.max_rows', None)
 
 # Display the updated DataFrame with daily price changes
 print(palantir_price_data_df)
