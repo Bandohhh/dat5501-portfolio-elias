bc1a486c7274d7a43a4b63b9122b539d40f7f641
Elias Bandoh <ebandoh27@gmail.com>
2025-11-11 19:13:10 +0000

    Function to calculate the duration between days

--- week_5/duration_calc.py
+++ week_5/duration_calc.py
@@ -2,3 +2,11 @@ import numpy as np
 import re
 date_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$") # regular expression pattern for YYYY-MM-DD format ensures 4 digits-2 digits-2 digits (integers only)
 
+def calculate_duration_in_days():
+    """
+    Calculate the duration in days between two dates.
+
+    Returns:
+    int: The duration in days between the two dates.
+    """
+    today = np.datetime64('today', 'D')
