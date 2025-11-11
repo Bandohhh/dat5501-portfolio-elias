29799cfca9d645a49ea46ff7defcbc220c4ab815
Elias Bandoh <ebandoh27@gmail.com>
2025-11-11 19:12:20 +0000

    Duration Calculator - converting the date format

---
+++ week_5/duration_calc.py
@@ -0,0 +1,4 @@
+import numpy as np
+import re
+date_pattern = re.compile(r"^\d{4}-\d{2}-\d{2}$") # regular expression pattern for YYYY-MM-DD format ensures 4 digits-2 digits-2 digits (integers only)
+
