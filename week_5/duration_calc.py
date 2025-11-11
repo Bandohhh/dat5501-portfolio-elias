48417b9b70da6aefb5d2b6eee9450079afdb4c34
Elias Bandoh <ebandoh27@gmail.com>
2025-11-11 19:24:53 +0000

    Initating a while loop that will ask user for a date and will not start the function until the date is entered

--- week_5/duration_calc.py
+++ week_5/duration_calc.py
@@ -10,3 +10,20 @@ def calculate_duration_in_days():
     int: The duration in days between the two dates.
     """
     today = np.datetime64('today', 'D')
+    while True:
+    # Loop until valid start date is entered
+        start_date_str = input("Please enter the start date (YYYY-MM-DD): ")
+        
+        if not date_pattern.match(start_date_str): # Validate start date format using regex
+            print("Invalid date format. Please enter dates in YYYY-MM-DD format.")
+            continue
+
+        try:
+            # Validate date values
+            start_date = np.datetime64(start_date_str, 'D')
+            
+            # Exit loop if start date is valid
+            break
+        
+        except ValueError: # Catch invalid date values
+            print("Invalid date format. Please enter dates in YYYY-MM-DD format.")
